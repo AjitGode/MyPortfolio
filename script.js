cb4abf4330b3e16b18080f83d061c9ba414bfb5f
ajit2599 <godeajit71@gmail.com>
2023-07-17 19:06:18 +0530

    Update Portfolio

--- script.js
+++ script.js
@@ -75,6 +75,28 @@ ScrollReveal({
     window.addEventListener("load",function(){
         loader.style.display="none";
     })
+    //scrolltop button
+    const scrollTop = document.getElementById('scrolltop')
+
+// When the page is loaded, hide the scroll-to-top button
+window.onload = () => {
+    scrollTop.style.visibility = "hidden";
+    scrollTop.style.opacity = 0;
+}
+
+// If the page is scrolled more than 200px,
+// show the scroll-to-top button
+// Otherwise hide the button
+window.onscroll = () => {
+    if (window.scrollY > 200) {
+        scrollTop.style.visibility = "visible";
+        scrollTop.style.opacity = 1;
+    } else {
+        scrollTop.style.visibility = "hidden";
+        scrollTop.style.opacity = 0;
+    }
+};
+
     // send email function
     
     function sendEmail(){
